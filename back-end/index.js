85b098980404d3f37a707f2702faf8a073ad1d46
thiuwinKarthik <thiruwinkarthik4@gmail.com>
2025-02-02 10:47:34 +0530

    Update index.js

--- back-end/index.js
+++ back-end/index.js
@@ -10,7 +10,7 @@ dotenv.config()
 app.use(express.json())
 app.use(cors( 
 {
-        origin: [""],
+        origin: ["https://mern-stack-iota-murex.vercel.app/"],
         methods: ["POST","GET"],
         credentials:true
     }
