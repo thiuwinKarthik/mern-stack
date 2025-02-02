74dc23a1b2553a4c1b24668d664c0735fcb9491c
thiuwinKarthik <thiruwinkarthik4@gmail.com>
2025-02-02 11:19:29 +0530

    Update index.js

--- back-end/index.js
+++ back-end/index.js
@@ -8,19 +8,19 @@ const bodyParser = require('body-parser')
 const app = express()
 dotenv.config()
 app.use(express.json())
-// app.use(cors( 
-// {
-//         origin: ["https://mern-stack-iota-murex.vercel.app"],
-//         methods: ["POST","GET"],
-//         credentials:true,
-//     }
-// ))
+app.use(cors( 
+{
+        origin: [""],
+        methods: ["POST","GET"],
+        credentials:true,
+    }
+))
 app.use(bodyParser.json())
 
 app.use('/api/products',router)
-app.get('/api/products',(req,res)=>{
-res.json("hello");
-}
+// app.get('/api/products',(req,res)=>{
+// res.json("hello");
+// }
 
 app.listen(process.env.port,()=>{
     console.log(`server started running on ${process.env.port}`)
